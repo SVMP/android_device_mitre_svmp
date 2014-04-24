898d09fa4cd62cf18783bf335b77fba8d524e052
David Keppler <dkeppler@mitre.org>
2014-04-24 11:40:21 -0400

    Fix FPS calculation in fbset
    fbset now correctly sets the FPS at all screen resolutions
    
    Change-Id: Iec9146ba472f1d819fae2158f060601d9e1e8126

--- fbset/Android.mk
+++ fbset/Android.mk
@@ -6,4 +6,6 @@ LOCAL_SRC_FILES := fbset.c
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE := fbset
 
+LOCAL_CFLAGS:= -DLOG_TAG=\"fbset\"
+
 include $(BUILD_EXECUTABLE)
--- fbset/fbset.c
+++ fbset/fbset.c
@@ -19,6 +19,7 @@ limitations under the License.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -30,16 +31,19 @@ limitations under the License.
 
 #define FBPATH "/dev/graphics/fb0"
 
+#include <cutils/log.h>
+
 void usage() {
-	printf ("usage: fbset X-res Y-res fps: i.e. fbset 720 1184 130000");
+	printf ("usage: fbset X-res Y-res fps: i.e. fbset 720 1184 30");
 	exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char** argv) {
 	int fd; 	
-	struct fb_var_screeninfo var;
+	struct fb_var_screeninfo info;
 	struct fb_fix_screeninfo fix;
-        float fps;
+	int targetFPS;
+        uint64_t refreshDivisor;
 
 	if (argc < 3) 
 		usage();
@@ -51,9 +55,8 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-
-	if(ioctl(fd, FBIOGET_VSCREENINFO, &var)) {
-		perror("Could not get variable screen info");
+	if(ioctl(fd, FBIOGET_VSCREENINFO, &info)) {
+		perror("Could not get infoiable screen info");
 		return 1;
 	}
 
@@ -62,101 +65,161 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	printf("fix: %16s (%d), type=%d/%d, visual=%d, pansteps=%d,%d\n", fix.id, fix.smem_len, fix.type, fix.type_aux, fix.visual, fix.xpanstep, fix.ypanstep);
-	printf("var: %dx%d visible, %dx%d virtual, %dx%d mm, %d,%d offset, %d bits/pixel, %d grayscale, %x vmode\n", var.xres, var.yres, var.xres_virtual, var.yres_virtual, var.width, var.height, var.xoffset, var.yoffset, var.bits_per_pixel, var.grayscale, var.vmode);
-	printf("colors: red: %d<<%d, green: %d<<%d, blue: %d<<%d, alpha; %d<<%d\n", var.red.length, var.red.offset, var.green.length, var.green.offset, var.blue.length, var.blue.offset, var.transp.length, var.transp.offset);
+	printf("fix: %16s (%d), type=%d/%d, visual=%d, pansteps=%d,%d\n",
+		fix.id, fix.smem_len, fix.type,
+		fix.type_aux, fix.visual,
+		fix.xpanstep, fix.ypanstep);
+	printf("info: %dx%d visible, %dx%d virtual, %dx%d mm, %d,%d "
+		"offset, %d bits/pixel, %d grayscale, %x vmode\n",
+		info.xres, info.yres,
+		info.xres_virtual, info.yres_virtual,
+		info.width, info.height,
+		info.xoffset, info.yoffset,
+		info.bits_per_pixel, info.grayscale, info.vmode);
+	printf("colors: red: %d<<%d, green: %d<<%d, blue: %d<<%d, alpha; %d<<%d\n",
+		info.red.length, info.red.offset,
+		info.green.length, info.green.offset,
+		info.blue.length, info.blue.offset,
+		info.transp.length, info.transp.offset);
 
-	// original setting
-	// 1280 x 768
-	var.xres = atoi(argv[1]);
-	var.yres = atof(argv[2]);
-	// new setting 360X480
-	//var.xres = 480;
-	//var.yres = 800;
+        // Set the frame resolution
+	info.xres = atoi(argv[1]);
+	info.yres = atoi(argv[2]);
 
-	var.xres_virtual = var.xres; 
-	// needed for page flipping
-	var.yres_virtual = 3 * var.yres;
+	info.xres_virtual = info.xres; 
+	// allow for triple buffering page flipping
+	info.yres_virtual = 3 * info.yres;
 //
 // 1/4/2012 -- use defaults in gralloc/framebuffer.cpp
 //
 /*
  *
- *        // must specify real width and height for DPI-detection purposes
- *        var.width = 52;	// 52mm, roughly the same as the Galaxy S screen
- *        var.height = var.width * var.yres / var.xres;
- *        
+ *	// must specify real width and height for DPI-detection purposes
+ *	info.width = 52;	// 52mm, roughly the same as the Galaxy S screen
+ *	info.height = info.width * info.yres / info.xres;
+ *	
  *
  */
 
-	var.yoffset = var.yres;
+	info.yoffset = info.yres;
 
-        fps = atoi(argv[3]);
-        var.pixclock = (int) ((( 1000000000.0 / fps) - 9000000.0) / 184.85);
+	/* Set the refresh timings.
+	 * ============================================================
+	 *
+	 * According to the kernel's Documentation/fb/framebuffer.txt:
+	 *   The frame buffer device uses the following fields:
+	 *
+	 *    - pixclock: pixel clock in ps (pico seconds)
+	 *    - left_margin: time from sync to picture
+	 *    - right_margin: time from picture to sync
+	 *    - upper_margin: time from sync to picture
+	 *    - lower_margin: time from picture to sync
+	 *    - hsync_len: length of horizontal sync
+	 *    - vsync_len: length of vertical sync
+	 *
+	 */
 
-	var.bits_per_pixel=16;
-	var.grayscale=0;
+	// gralloc/framebuffer.cpp calculates the fps as:
+	//
+	// uint64_t  refreshQuotient =
+	// (
+	//	 uint64_t( info.upper_margin + info.lower_margin + info.yres )
+	//	 * ( info.left_margin  + info.right_margin + info.xres )
+	//	 * info.pixclock
+	// );
+	// 
+	// /* Beware, info.pixclock might be 0 under emulation, so avoid a
+	//  * division-by-0 here (SIGFPE on ARM) */
+	// int refreshRate = refreshQuotient > 0 ? (int)(1000000000000000LLU / refreshQuotient) : 0;
+	//
+	// float fps  = refreshRate / 1000.0f;
+
+	targetFPS = atoi(argv[3]);
+        refreshDivisor =
+	(
+		(uint64_t)( info.upper_margin + info.lower_margin + info.yres )
+			* ( info.left_margin + info.right_margin + info.xres )
+			* targetFPS
+	);
+	info.pixclock = (uint32_t)( 1000000000000LLU / refreshDivisor );
+
+	/* ========================================================== */
+
+	info.bits_per_pixel=16;
+	info.grayscale=0;
 	// only handle 16 bits / per pixel at the moment
 	// http://www.linux-fbdev.org/HOWTO/4.html
 	
-	switch (var.bits_per_pixel) {
+	switch (info.bits_per_pixel) {
 		case 1:
 		case 8:
 			/* Pseudocolor mode example */
-			var.red.offset    = 0;
-			var.red.length    = 8;
-			var.green.offset  = 0;
-			var.green.length  = 8;
-			var.blue.offset   = 0;
-			var.blue.length   = 8;
-			var.transp.offset = 0;
-			var.transp.length = 0;
+			info.red.offset    = 0;
+			info.red.length    = 8;
+			info.green.offset  = 0;
+			info.green.length  = 8;
+			info.blue.offset   = 0;
+			info.blue.length   = 8;
+			info.transp.offset = 0;
+			info.transp.length = 0;
 			break;
-		case 16:        /* RGB 565 */
-			var.red.offset    = 0;
-			var.red.length    = 5;
-			var.green.offset  = 5;
-			var.green.length  = 6;
-			var.blue.offset   = 11;
-			var.blue.length   = 5;
-			var.transp.offset = 0;
-			var.transp.length = 0;
+		case 16:	/* RGB 565 */
+			info.red.offset    = 0;
+			info.red.length    = 5;
+			info.green.offset  = 5;
+			info.green.length  = 6;
+			info.blue.offset   = 11;
+			info.blue.length   = 5;
+			info.transp.offset = 0;
+			info.transp.length = 0;
 			break;
-		case 24:        /* RGB 888 */
-			var.red.offset    = 0;
-			var.red.length    = 8;
-			var.green.offset  = 8;
-			var.green.length  = 8;
-			var.blue.offset   = 16;
-			var.blue.length   = 8;
-			var.transp.offset = 0;
-			var.transp.length = 0;
+		case 24:	/* RGB 888 */
+			info.red.offset    = 0;
+			info.red.length    = 8;
+			info.green.offset  = 8;
+			info.green.length  = 8;
+			info.blue.offset   = 16;
+			info.blue.length   = 8;
+			info.transp.offset = 0;
+			info.transp.length = 0;
 			break;
-		case 32:        /* RGBA 8888 */
-			var.red.offset    = 0;
-			var.red.length    = 8;
-			var.green.offset  = 8;
-			var.green.length  = 8;
-			var.blue.offset   = 16;
-			var.blue.length   = 8;
-			var.transp.offset = 24;
-			var.transp.length = 8;
+		case 32:	/* RGBA 8888 */
+			info.red.offset    = 0;
+			info.red.length    = 8;
+			info.green.offset  = 8;
+			info.green.length  = 8;
+			info.blue.offset   = 16;
+			info.blue.length   = 8;
+			info.transp.offset = 24;
+			info.transp.length = 8;
 			break;
 	}
 
-		var.vmode = FB_VMODE_YWRAP;
+	info.vmode = FB_VMODE_YWRAP;
 
-	if(ioctl(fd, FBIOPUT_VSCREENINFO, &var)) {
-		perror("Could not set variable screen info");
+	// Set the new screeninfo 
+	if(ioctl(fd, FBIOPUT_VSCREENINFO, &info)) {
+		perror("Could not set screen info");
 		return 1;
 	}
 
-	if(ioctl(fd, FBIOGET_VSCREENINFO, &var)) {
-		perror("Could not reget variable screen info");
+	// Get the screeninfo again to double check the values
+	if(ioctl(fd, FBIOGET_VSCREENINFO, &info)) {
+		perror("Could not reget screen info");
 		return 1;
 	}
-	printf("var: %dx%d visible, %dx%d virtual, %dx%d mm, %d,%d offset, %d bits/pixel, %d grayscale, %x vmode\n", var.xres, var.yres, var.xres_virtual, var.yres_virtual, var.width, var.height, var.xoffset, var.yoffset, var.bits_per_pixel, var.grayscale, var.vmode);
-	printf("colors: red: %d<<%d, green: %d<<%d, blue: %d<<%d, alpha; %d<<%d\n", var.red.length, var.red.offset, var.green.length, var.green.offset, var.blue.length, var.blue.offset, var.transp.length, var.transp.offset);
+	printf("info: %dx%d visible, %dx%d virtual, %dx%d mm, %d,%d offset, %d bits/pixel, %d grayscale, %x vmode\n", 
+		info.xres, info.yres,
+		info.xres_virtual, info.yres_virtual,
+		info.width, info.height,
+		info.xoffset, info.yoffset,
+		info.bits_per_pixel,
+		info.grayscale, info.vmode);
+	printf("colors: red: %d<<%d, green: %d<<%d, blue: %d<<%d, alpha; %d<<%d\n",
+		info.red.length, info.red.offset,
+		info.green.length, info.green.offset,
+		info.blue.length, info.blue.offset,
+		info.transp.length, info.transp.offset);
 
 	return 0;
 }
